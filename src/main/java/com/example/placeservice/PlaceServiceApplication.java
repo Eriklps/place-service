7938d1df0b8426fb97361e443d40a6d6f4270cc1
Eriklps <erikdslopes@gmail.com>
2024-03-05 09:25:45 -0300

    Initial commit

--- src/main/java/com/example/placeservice/PlaceServiceApplication.java
+++ src/main/java/com/example/placeservice/PlaceServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.realestate;
+package com.example.placeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RealEstateApplication {
+public class PlaceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RealEstateApplication.class, args);
+		SpringApplication.run(PlaceServiceApplication.class, args);
 	}
 
 }
