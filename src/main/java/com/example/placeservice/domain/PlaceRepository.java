051c31fc7de379e03de36d16b0d06e5037be28ed
Eriklps <erikdslopes@gmail.com>
2024-03-05 09:39:06 -0300

    Implemented the domain layer

---
+++ src/main/java/com/example/placeservice/domain/PlaceRepository.java
@@ -0,0 +1,6 @@
+package com.example.placeservice.domain;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface PlaceRepository extends ReactiveCrudRepository<Place, Long> {
+}
